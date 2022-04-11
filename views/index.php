03c297082c17bf76812e3ef2eb9e0a9038194ece
Robin Hood <yoga3911@gmail.com>
2022-04-11 18:49:37 +0700

    fix navbar

--- views/index.php
+++ views/index.php
@@ -9,34 +9,33 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;900&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="../styles/style.css">
     <title>Produk</title>
-    <style>
-        * {
-            font-family: 'Poppins', sans-serif;
-            font-weight: 900;
-        }
-    </style>
 </head>
 
 <body>
-    <div class="container-fluid">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Navbar</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    </ul>
-                    <form class="d-flex">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-outline-success" type="submit">Search</button>
+    <nav class="navbar navbar-expand-sm navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#"><span class="logo" style="color: white;">Hi</span><span class="logo" style="color: #059BE5">Tech</span></a>
+            <button class="navbar-toggler" style="filter: grayscale(1) invert(1);" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <div class="left-component d-flex me-0 align-items-center">
+                    <form>
+                        <div class="input-group">
+                            <span class="input-group-text" id="basic-addon1" style="background-color: white;"><img src="../icons/search.svg" alt="" height="20"></span>
+                            <input class="form-control ms-auto p-0 bd-highlight me-2" type="search" placeholder="Search" aria-label="Search">
+                        </div>
                     </form>
+                    <img class="mx-1" height="28" src="../icons/favorite.svg" alt="favorite">
+                    <img class="mx-1" height="30" src="../icons/cart.svg" alt="cart">
+                    <img class="mx-1" height="36" src="../icons/person.svg" alt="person">
                 </div>
             </div>
-        </nav>
-    </div>
+        </div>
+    </nav>
+    <script src="https://cdn.jsdelivr.net/gh/xcash/bootstrap-autocomplete@v2.3.7/dist/latest/bootstrap-autocomplete.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 
