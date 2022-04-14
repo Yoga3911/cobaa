c338b4f01b7aa4a95e81c8911242faa3799bc264
Robin Hood <yoga3911@gmail.com>
2022-04-14 08:37:14 +0700

    fix wrong margin position

--- views/index.php
+++ views/index.php
@@ -10,7 +10,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;900&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="../styles/style.css">
+    <link rel="stylesheet" href="../styles/styles.css">
     <title>Produk</title>
 </head>
 
@@ -23,7 +23,7 @@
                     <span class="navbar-toggler-icon"></span>
                 </button>
             </div>
-            <div class="title2 col-12 col-sm-9 col-md-7 col-lg-5">
+            <div class="title2 col-12 col-sm-9 col-md-7 col-lg-5 mx-5">
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <div class="d-flex align-items-center justify-content-end">
                         <form class="mx-4">
